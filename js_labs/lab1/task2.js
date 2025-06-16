dbfb44145c69c840fe1baea31175c61978f6f953
m0rvo <vovam387@gmail.com>
2025-06-16 10:10:31 +0300

    Update task2.js

--- js_labs/lab1/task2.js
+++ js_labs/lab1/task2.js
@@ -1,3 +1,4 @@
+ 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
     const checkButton = document.getElementById('checkButton');
     const numberInput = document.getElementById('numberInput');
